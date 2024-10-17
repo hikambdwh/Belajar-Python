051bfaad57ed702a564c2f150a3c4079d6918866
hikambdwh <hikambaidawih@gmail.com>
2024-10-18 00:52:10 +0700

    memodifikasi

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-age = 18
-nama = "hikam"
-print(f"nama kamu {nama} \numur kamu {age} tahun")
+age = 18;
+nama = "hikam";
+print(f"nama kamu {nama} \numur kamu {age} tahun");
 
+x = int(input("x = "))
+y = int(input("y = "))
+z = y / x
+print(f"y / x = {z}")
