94f17ec1e1f0f4fc524ca3979e6875838f2e2e9e
hikambdwh <hikambaidawih@gmail.com>
2024-10-26 16:52:46 +0700

    Menambah beberapa file pembelajaran python

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 age = 18;
 nama = "hikam";
-print(f"nama kamu {nama} \numur kamu {age} tahun");
+kalimat = f"Nama kamu {nama}"
+print(f"{kalimat}\numur kamu {age} tahun");
 
 x = int(input("x = "))
 y = int(input("y = "))
+a = x >= y
+b = not (x >= y)
+v = a | b
 z = y / x
+
+print(a, b, v)
 print(f"y / x = {z}")
