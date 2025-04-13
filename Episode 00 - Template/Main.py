004007f3c66e7a3565cd7f1669466f592cce61ec
hikambdwh <hikambaidawih@gmail.com>
2025-04-13 18:52:07 +0700

    mengganti nilai

--- Episode 00 - Template/Main.py
+++ Episode 00 - Template/Main.py
@@ -1,3 +1,24 @@
 print("helo dunia!!!!!!!")
 print("apa kabar kalian?")
 print("instalasi berhasil")
+
+angka = 12
+print(f"umur {angka}")
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import sys
+
+def demo():
+    app=QApplication(sys.argv)
+    win=QMainWindow()
+    win.setGeometry(300,300,300,300)
+    win.setWindowTitle("Demo app")
+    label=QtWidgets.QLabel(win)
+    label.setText("Hikam tamvan")
+    label.move(100,100)
+
+    win.show()
+    sys.exit(app.exec_())
+
+demo()
